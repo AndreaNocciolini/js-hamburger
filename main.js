ebb6ea6c56b9d13659dd52373d187474e029971e
AndreaNocciolini <andreanocciolini93@gmail.com>
2021-11-22 15:06:37 +0100

    finish

---
+++ main.js
@@ -0,0 +1,17 @@
+// Variables
+const hamburgerMenuOpen = document.querySelector(`.fas.fa-bars`)
+const hamburgerMenuContent = document.querySelector(`.hamburger-menu`)
+const hamburgerMenuClose = document.querySelector(`.fas.fa-times`)
+
+// console.log(hamburgerMenuOpen, hamburgerMenuContent, hamburgerMenuClose);
+// End Variables 
+
+// Open Hamburger Menù
+hamburgerMenuOpen.addEventListener(`click`, function(){
+    hamburgerMenuContent.classList.add(`active`);
+})
+
+// Close Hamburger Menù
+hamburgerMenuClose.addEventListener(`click`, function(){
+    hamburgerMenuContent.classList.remove(`active`);
+})
