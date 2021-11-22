0bc78c995311f698b308ccf90064de0707f6b881
AndreaNocciolini <andreanocciolini93@gmail.com>
2021-11-22 15:58:37 +0100

    miscellaneous

--- BONUS/main.js
+++ BONUS/main.js
@@ -3,21 +3,22 @@ const hamburgerMenuOpen = document.querySelector(`.header-right a i`);
 const hamburgerMenuContent = document.querySelector(`.hamburger-menu`);
 const hamburgerMenuClose = document.querySelector(`.close`);
 
-// EXERCISE
-// ***********************************************************
-// // Open Hamburger Menù                                    *
-// hamburgerMenuOpen.addEventListener(`click`, function(){   *
-//     hamburgerMenuContent.classList.add(`active`);         *
-// })                                                        *
-//                                                           *
-// // Close Hamburger Menù                                   *
-// hamburgerMenuClose.addEventListener(`click`, function(){  *
-//     hamburgerMenuContent.classList.remove(`active`);      *
-// })                                                        *
-// ***********************************************************
-// END EXERCISE
+// EXERCISE*********************************************************
+// *****************************************************************
+// // Open Hamburger Menù                                    *******
+// hamburgerMenuOpen.addEventListener(`click`, function(){   *******
+//     hamburgerMenuContent.classList.add(`active`);         *******
+// })                                                        *******
+//                                                           *******
+// // Close Hamburger Menù                                   *******
+// hamburgerMenuClose.addEventListener(`click`, function(){  *******
+//     hamburgerMenuContent.classList.remove(`active`);      *******
+// })                                                        *******
+// *****************************************************************
+// END EXERCISE*****************************************************
 
-// BONUS .className
+//************************************************************************************
+// BONUS .className 
 // // Open Hamburger Menù
 // hamburgerMenuOpen.addEventListener('click', function(){
 //     hamburgerMenuContent.className = hamburgerMenuContent.classList + " active";
@@ -28,7 +29,9 @@ const hamburgerMenuClose = document.querySelector(`.close`);
 //     hamburgerMenuContent.classList.remove(`active`);
 // })
 // END BONUS .className
+//************************************************************************************
 
+//************************************************************************************
 // BONUS .style
 // Open Hamburger Menù
 hamburgerMenuOpen.addEventListener(`click`, function(){
@@ -39,3 +42,5 @@ hamburgerMenuOpen.addEventListener(`click`, function(){
 hamburgerMenuClose.addEventListener(`click`, function(){
     hamburgerMenuContent.style.display = "none";
 })
+// END BONUS .style
+//************************************************************************************
